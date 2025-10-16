f2145060d51c52b01dcddb9ab1063949b6f31de4
Shaurtya deep shukla <shauryadeep589@gmail.com>
2025-10-16 17:58:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import "./App.css"
+import Footer from "./components/Footer";
+function App() {
+  return (
+    <>
+    <Navbar/>
+     <Hero></Hero>
+<Footer></Footer>
+    </>
+  );
+}
+
+export default App;
